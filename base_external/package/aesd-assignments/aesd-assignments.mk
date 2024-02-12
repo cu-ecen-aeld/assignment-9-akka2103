c98f742710dec3bd6cdc6427e09df6006b85ecf1
akash <akash@ubuntu20.4.myguest.virtualbox.org>
2024-02-11 17:37:14 -0700

    Add buildroot submodule

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 7c99baf400f668de766161482d989352b7c503cb
+AESD_ASSIGNMENTS_VERSION = 0299e8018f7d9d8f09f3bcf1ef97df973a98db6c
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -23,6 +23,11 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+
+	# Install writer, finder, and finder-test binaries
+	$(INSTALL) -d 0755 $(@D)/finder-app/writer $(TARGET_DIR)/user/bin
+	$(INSTALL) -d 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/user/bin
+	$(INSTALL) -d 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/user/bin
 endef
 
 $(eval $(generic-package))
