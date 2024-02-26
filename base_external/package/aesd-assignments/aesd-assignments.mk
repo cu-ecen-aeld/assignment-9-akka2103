7ddd71bd7b7287ad5c2c7f6325245cbbe45bffaf
akash <akash@ubuntu20.4.myguest.virtualbox.org>
2024-02-25 18:15:10 -0700

    changed commit hash

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 9ce27ce8e8e858d6de48a557c9982b48ff823c75
+AESD_ASSIGNMENTS_VERSION = c6a748480cd1c14f41ccb5e4b04d11ac5a46d2b5
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
