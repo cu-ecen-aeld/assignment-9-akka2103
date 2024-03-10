e50e25bebe114545da6a006add99c6081cf5872c
akash <akash@ubuntu20.4.myguest.virtualbox.org>
2024-03-10 17:19:53 -0600

    test changes

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -8,7 +8,7 @@
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 AESD_ASSIGNMENTS_VERSION = 0c950529e2083cb18533c7badff01ac18cd6becf 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
-# with ssh keys and the automated build/test system.
+# with ssh keys and automated build/test system.
 # Your site should start with git@github.com:
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-akka2103.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
