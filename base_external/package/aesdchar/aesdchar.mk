6ffa2c0f16450aaec70addf4b0bcdd11c2c1dac6
akka2103 <143990610+akka2103@users.noreply.github.com>
2024-04-02 14:25:42 -0600

    Update aesdchar.mk

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESDCHAR_VERSION = 0e88db1c07a2f5535f21cbc88325e8d5eb8da81f
+AESDCHAR_VERSION = 78aeefaf2ae680c8ba64f0bec0962220d7fcd5d0
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
