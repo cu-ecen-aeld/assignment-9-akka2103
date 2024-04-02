4a67a7d3e275c305511e72958a39800ea348d2f4
akka2103 <143990610+akka2103@users.noreply.github.com>
2024-04-02 14:25:15 -0600

    Update aesd-assignments.mk

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 0c950529e2083cb18533c7badff01ac18cd6becf 
+AESD_ASSIGNMENTS_VERSION = 78aeefaf2ae680c8ba64f0bec0962220d7fcd5d0 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and automated build/test system.
 # Your site should start with git@github.com:
